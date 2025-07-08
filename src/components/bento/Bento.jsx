d86942210efd933a4cde2988e3930a44d480119e
skrzeczek <h.skrzeczek@gmail.com>
2025-07-08 23:03:29 +0200

    frontend: initial aligment fix

--- src/components/bento/Bento.jsx
+++ src/components/bento/Bento.jsx
@@ -5,9 +5,11 @@ import BeanArea from '../bean/BeanArea';
 const Bento = () => {
     return (
         <div className="bento-container">
-            <Card className="card large-card" title="Full interest" content=" in web development. My main goal is join design and performance.">
-                <img src="/assets/figmaImg.png" alt="img" className="card-img" />
-            </Card>
+            <div className="margin-fix">
+                <Card className="card large-card" title="Full interest" content=" in web development. My main goal is join design and performance.">
+                    <img src="/assets/figmaImg.png" alt="img" className="card-img" />
+                </Card>
+            </div>
             <div className="bento-wrapper">
                 <div className="bento-inner-wrapper">
                     <Card className="card medium-card" title="Strong problem-solving " content="mindset, with keen eye for detail." orientation='column'>
