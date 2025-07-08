e17a16afea28c8053dc14af6f52e047d5420ddda
skrzeczek <h.skrzeczek@gmail.com>
2025-07-08 23:20:28 +0200

    frontend: bento fix

--- src/components/bento/Bento.jsx
+++ src/components/bento/Bento.jsx
@@ -6,13 +6,13 @@ const Bento = () => {
     return (
         <div className="bento-container">
             <div className="margin-fix">
-                <Card className="card large-card" title="Full interest" content=" in web development. My main goal is join design and performance.">
+                <Card title="Full interest" content=" in web development. My main goal is join design and performance.">
                     <img src="/assets/figmaImg.png" alt="img" className="card-img" />
                 </Card>
             </div>
             <div className="bento-wrapper">
                 <div className="bento-inner-wrapper">
-                    <Card className="card medium-card" title="Strong problem-solving " content="mindset, with keen eye for detail." orientation='column'>
+                    <Card title="Strong problem-solving " content="mindset, with keen eye for detail." orientation='column'>
                         <img src="/assets/problemImg.png" alt="img" className="card-img" />
                     </Card>
                 </div>
@@ -21,7 +21,7 @@ const Bento = () => {
                         <BeanArea />
                     </div>
                     <div>
-                        <Card className="card small-card" title="Creative, " content="all the time finding new solutions.">
+                        <Card title="Creative, " content="all the time finding new solutions.">
                             <img src="/assets/creativeImg.png" alt="img" className="card-img" />
                         </Card>
                     </div>
